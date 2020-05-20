53a5d75e98348bedf1be4714f8decfe08e8a5c78
ogLankyBoi <61032869+ogLankyBoi@users.noreply.github.com>
2020-05-19 22:52:40 -0400

    Added game handler
    Creates deck and shuffles, added deal function but doesn't work yet

---
+++ IrishPokerCardGame/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    public Sprite[] cardFaces;
+    public GameObject cardPrefab;
+
+    public static string[] suits = new string[] { "C", "D", "H", "S" };
+    public static string[] values = new string[] { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+
+    public List<string> deck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPlaying();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startPlaying()
+    {
+        deck = GenerateDeck();
+        ShuffleDeck(deck);
+        //Deal(deck);
+    }
+
+    public static List<string> GenerateDeck()
+    {
+        List<string> newDeck = new List<string>();
+        foreach (string s in suits)
+        {
+            foreach (string v in values)
+            {
+                newDeck.Add(v + s);
+            }
+        }
+
+        return newDeck;
+    }
+
+    void ShuffleDeck<T>(List<T> list)
+    {
+        System.Random random = new System.Random();
+        int n = list.Count;
+        while (n > 1)
+        {
+            int k = random.Next(n);
+            n--;
+            T temp = list[k];
+            list[k] = list[n];
+            list[n] = temp;
+        }
+    }
+
+/*    void Deal(string[] deck)
+    {
+        float xOffset = 0;
+        for(int i = 0; i < 4; i++)
+        {
+            GameObject newCard = Instantiate(cardPrefab, new Vector3(transform.position.x + xOffset, transform.position.y, transform.position.z), Quaternion.identity);
+            newCard.name = deck[i];
+        }
+    }*/
+}
