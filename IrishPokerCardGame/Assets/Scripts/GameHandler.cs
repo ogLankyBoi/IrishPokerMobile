6640be38041e230e211c43e6669646168d1bda7a
ogLankyBoi <61032869+ogLankyBoi@users.noreply.github.com>
2020-05-20 21:12:56 -0400

    Complete the rest of the four cards

--- IrishPokerCardGame/Assets/Scripts/GameHandler.cs
+++ IrishPokerCardGame/Assets/Scripts/GameHandler.cs
@@ -113,6 +113,14 @@ public class GameHandler : MonoBehaviour
         {
             higherOrLower(1);
         }
+        else if (round == 3)
+        {
+            outOrIn(1);
+        }
+        else if (round == 4)
+        {
+            whatSuit(1);
+        }
     }
 
     public void OnButton2()
@@ -125,16 +133,30 @@ public class GameHandler : MonoBehaviour
         {
             higherOrLower(2);
         }
+        else if (round == 3)
+        {
+            outOrIn(2);
+        }
+        else if (round == 4)
+        {
+            whatSuit(2);
+        }
     }
 
     public void OnButton3()
     {
-
+        if (round == 4)
+        {
+            whatSuit(3);
+        }
     }
 
     public void OnButton4()
     {
-
+        if (round == 4)
+        {
+            whatSuit(4);
+        }
     }
 
     public void redOrBlack(int buttonNum)
@@ -216,43 +238,43 @@ public class GameHandler : MonoBehaviour
         switch (deck[1][0])
         {
             case '2':
-                card1Value = 2;
+                card2Value = 2;
                 break;
             case '3':
-                card1Value = 3;
+                card2Value = 3;
                 break;
             case '4':
-                card1Value = 4;
+                card2Value = 4;
                 break;
             case '5':
-                card1Value = 5;
+                card2Value = 5;
                 break;
             case '6':
-                card1Value = 6;
+                card2Value = 6;
                 break;
             case '7':
-                card1Value = 7;
+                card2Value = 7;
                 break;
             case '8':
-                card1Value = 8;
+                card2Value = 8;
                 break;
             case '9':
-                card1Value = 9;
+                card2Value = 9;
                 break;
             case 'T':
-                card1Value = 10;
+                card2Value = 10;
                 break;
             case 'J':
-                card1Value = 11;
+                card2Value = 11;
                 break;
             case 'Q':
-                card1Value = 12;
+                card2Value = 12;
                 break;
             case 'K':
-                card1Value = 13;
+                card2Value = 13;
                 break;
             default:
-                card1Value = 14;
+                card2Value = 14;
                 break;
         }
 
@@ -290,5 +312,199 @@ public class GameHandler : MonoBehaviour
         button1.GetComponentInChildren<Text>().text = "Outside";
         button2.GetComponentInChildren<Text>().text = "Inside";
         round++;
+    }
+
+    public void outOrIn(int buttonNum)
+    {
+        int highCard;
+        int lowCard;
+        GameObject.Find(deck[2]).GetComponent<Seeable>().faceUp = true;
+        switch (deck[2][0])
+        {
+            case '2':
+                card3Value = 2;
+                break;
+            case '3':
+                card3Value = 3;
+                break;
+            case '4':
+                card3Value = 4;
+                break;
+            case '5':
+                card3Value = 5;
+                break;
+            case '6':
+                card3Value = 6;
+                break;
+            case '7':
+                card3Value = 7;
+                break;
+            case '8':
+                card3Value = 8;
+                break;
+            case '9':
+                card3Value = 9;
+                break;
+            case 'T':
+                card3Value = 10;
+                break;
+            case 'J':
+                card3Value = 11;
+                break;
+            case 'Q':
+                card3Value = 12;
+                break;
+            case 'K':
+                card3Value = 13;
+                break;
+            default:
+                card3Value = 14;
+                break;
+        }
+        if (card1Value >= card2Value)
+        {
+            highCard = card1Value;
+            lowCard = card2Value;
+        }
+        else
+        {
+            highCard = card2Value;
+            lowCard = card1Value;
+        }
+
+        if (buttonNum == 1)
+        {
+            if (card3Value > highCard && card3Value < lowCard)
+            {
+                dialogueText.GetComponent<Text>().text = "Correct! You don't have to drink." + Environment.NewLine + Environment.NewLine + "Hearts, Diamonds, Clubs, or Spades?";
+            }
+            else if (card3Value < highCard && card3Value > lowCard)
+            {
+                dialogueText.GetComponent<Text>().text = "You are wrong. Drink " + card3Value + " times" + Environment.NewLine + Environment.NewLine + "Hearts, Diamonds, Clubs, or Spades?";
+            }
+            else
+            {
+                dialogueText.GetComponent<Text>().text = "Big oof, they have the same value. Drink " + card3Value * 2 + " times" + Environment.NewLine + Environment.NewLine + "Hearts, Diamonds, Clubs, or Spades?";
+            }
+        }
+        else if (buttonNum == 2)
+        {
+            if (card3Value < highCard && card3Value > lowCard)
+            {
+                dialogueText.GetComponent<Text>().text = "Correct! You don't have to drink." + Environment.NewLine + Environment.NewLine + "Hearts, Diamonds, Clubs, or Spades?";
+            }
+            else if (card3Value > highCard && card3Value < lowCard)
+            {
+                dialogueText.GetComponent<Text>().text = "You are wrong. Drink " + card3Value + " times." + Environment.NewLine + Environment.NewLine + "Hearts, Diamonds, Clubs, or Spades?";
+            }
+            else
+            {
+                dialogueText.GetComponent<Text>().text = "Big oof, they have the same value. Drink " + card3Value * 2 + " times" + Environment.NewLine + Environment.NewLine + "Hearts, Diamonds, Clubs, or Spades?";
+            }
+        }
+
+        button3.SetActive(true);
+        button4.SetActive(true);
+        button1.GetComponentInChildren<Text>().text = "Hearts";
+        button2.GetComponentInChildren<Text>().text = "Diamonds";
+        button3.GetComponentInChildren<Text>().text = "Clubs";
+        button4.GetComponentInChildren<Text>().text = "Spades";
+        round++;
+    }
+
+    public void whatSuit(int buttonNum)
+    {
+        GameObject.Find(deck[3]).GetComponent<Seeable>().faceUp = true;
+        switch (deck[3][0])
+        {
+            case '2':
+                card4Value = 2;
+                break;
+            case '3':
+                card4Value = 3;
+                break;
+            case '4':
+                card4Value = 4;
+                break;
+            case '5':
+                card4Value = 5;
+                break;
+            case '6':
+                card4Value = 6;
+                break;
+            case '7':
+                card4Value = 7;
+                break;
+            case '8':
+                card4Value = 8;
+                break;
+            case '9':
+                card4Value = 9;
+                break;
+            case 'T':
+                card4Value = 10;
+                break;
+            case 'J':
+                card4Value = 11;
+                break;
+            case 'Q':
+                card4Value = 12;
+                break;
+            case 'K':
+                card4Value = 13;
+                break;
+            default:
+                card4Value = 14;
+                break;
+        }
+        if (buttonNum == 1)
+        {
+            if (deck[3][1] == 'H')
+            {
+                dialogueText.GetComponent<Text>().text = "Correct! You don't have to drink." + Environment.NewLine + Environment.NewLine + "You are done with the game.";
+            }
+            else
+            {
+                dialogueText.GetComponent<Text>().text = "You are wrong. Drink " + card4Value + " times" + Environment.NewLine + Environment.NewLine + "You are done with the game.";
+            }
+        }
+        else if (buttonNum == 2)
+        {
+            if (deck[3][1] == 'D')
+            {
+                dialogueText.GetComponent<Text>().text = "Correct! You don't have to drink." + Environment.NewLine + Environment.NewLine + "You are done with the game.";
+            }
+            else
+            {
+                dialogueText.GetComponent<Text>().text = "You are wrong. Drink " + card4Value + " times." + Environment.NewLine + Environment.NewLine + "You are done with the game.";
+            }
+        }
+        else if (buttonNum == 3)
+        {
+            if (deck[3][1] == 'C')
+            {
+                dialogueText.GetComponent<Text>().text = "Correct! You don't have to drink." + Environment.NewLine + Environment.NewLine + "You are done with the game.";
+            }
+            else
+            {
+                dialogueText.GetComponent<Text>().text = "You are wrong. Drink " + card4Value + " times." + Environment.NewLine + Environment.NewLine + "You are done with the game.";
+            }
+        }
+        else if (buttonNum == 4)
+        {
+            if (deck[3][1] == 'S')
+            {
+                dialogueText.GetComponent<Text>().text = "Correct! You don't have to drink." + Environment.NewLine + Environment.NewLine + "You are done with the game.";
+            }
+            else
+            {
+                dialogueText.GetComponent<Text>().text = "You are wrong. Drink " + card4Value + " times." + Environment.NewLine + Environment.NewLine + "You are done with the game.";
+            }
+        }
+
+        button1.SetActive(false);
+        button2.SetActive(false);
+        button3.SetActive(false);
+        button4.SetActive(false);
     }
 }
