f617824a605b6180c9c075182b931a7733d68ea2
ogLankyBoi <61032869+ogLankyBoi@users.noreply.github.com>
2020-05-21 19:00:45 -0400

    Added Scene Changing Functionality
    Added Button Functionality
    MainMenu -> Play Button takes user to PlayMenu
    PlayMenu -> Solo Button takes user to OnePersonGame and << Button takes user to MainMenu
    OnePlayerGame -> ? Button takes user back to MainMenu, but need to add a pop-up screen to allow user to adjust settings and exit if wanted.

--- IrishPokerCardGame/Assets/Scripts/GameHandler.cs
+++ IrishPokerCardGame/Assets/Scripts/GameHandler.cs
@@ -9,6 +9,7 @@ public class GameHandler : MonoBehaviour
 {
     public Sprite[] cardFaces;
     public GameObject cardPrefab;
+    public SceneChanger sceneChanger;
 
     public static string[] suits = new string[] { "C", "D", "H", "S" };
     public static string[] values = new string[] { "A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K" };
@@ -38,6 +39,7 @@ public class GameHandler : MonoBehaviour
 
     public void startPlaying()
     {
+        sceneChanger = GetComponent<SceneChanger>();
         dialogueText = GameObject.Find("DialogueText");
         setUpButtons();
         deck = GenerateDeck();
@@ -506,5 +508,10 @@ public class GameHandler : MonoBehaviour
         button2.SetActive(false);
         button3.SetActive(false);
         button4.SetActive(false);
+    }
+
+    public void OnInGameSettingsButton()
+    {
+        sceneChanger.SceneLoad("MainMenu");
     }
 }
---
+++ IrishPokerCardGame/Assets/Scripts/MenuHandlerScr.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHandlerScr : MonoBehaviour
+{
+
+    public SceneChanger sceneChanger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneChanger = GetComponent<SceneChanger>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPlayButton()
+    {
+        sceneChanger.SceneLoad("PlayMenu");
+    }
+
+    public void OnSettingsButton()
+    {
+        print("2");
+    }
+
+    public void OnHowToButton()
+    {
+        print("3");
+    }
+}
